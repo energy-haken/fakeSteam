ca933820594d6bb6d9cf41f1556b2aae93564368
Nathan REUTENAUER <nreutenauer@unistra.fr>
2024-11-13 11:56:29 +0100

    Count + list

---
+++ v0/operationsUtils.c
@@ -0,0 +1,20 @@
+#include "operationsUtils.h"
+
+
+int count(Jeu jeux[]){
+    int counter = 0 ;
+    
+    while(jeux[counter] != (Jeu)0 ){
+        counter +=1 ;
+        
+    }
+    return counter ;
+    
+}
+
+int list(Jeu jeux[]){
+    for(int i = 0 ; i < count(jeux) ; i++){
+        printf("%s \n", jeux[i].NomJeu  );
+    }
+}
+
