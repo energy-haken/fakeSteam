d47c106dea06e3ff7c4869e20fb83320fdbe9984
Florian S <contact@flo-x.fr>
2024-11-13 10:54:16 +0100

    Addes base structures

---
+++ v0/structures.h
@@ -0,0 +1,12 @@
+typedef struct
+{
+    int CodeOp;
+    char NomJeu[25];
+    char Param[200];
+    int flag;
+} DemandeOperation;
+
+typedef struct {
+    char NomJeu[25] ;
+    char *code;
+} Jeu ;
